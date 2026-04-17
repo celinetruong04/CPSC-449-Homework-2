baf25cb10c7062b7ed5c7cfde77cb941df2e42ac
celinetruong04 <celinetruong04@gmail.com>
2026-04-17 16:43:37 -0700

    added professor's files and made changes to meet assignment requirements

---
+++ bookstore_mongodb/src/main/java/com/example/bookstore_mongodb/controller/BookController.java
@@ -0,0 +1,40 @@
+package com.example.bookstore_mongodb.controller;
+
+import com.example.bookstore_mongodb.entity.Book;
+import com.example.bookstore_mongodb.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookService.createBook(book);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteBook(@PathVariable String id) {
+
+        if (!bookService.existsById(id)) {
+            return ResponseEntity.status(404).body(Map.of("error", "Book not found"));
+        }
+
+        bookService.deleteBookById(id);
+
+        return ResponseEntity.ok(Map.of("message", "Book deleted successfully"));
+    }
+
+}
